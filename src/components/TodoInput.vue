6a643f9497670f790f9855a4128c31dc8eb3b4c1
Kewon <48837736+kewon96@users.noreply.github.com>
2021-09-17 12:31:06 +0900

    convert setup

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -10,28 +10,53 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
 
-export default defineComponent({
-  name: 'TodoInput',
-  props: {
-    item: {
-      type: String,
-      required: true
-    }
-  },
-  methods: {
-    handleInput(event: InputEvent) {
-      // input tag에 있는 input Event하고 $emit에 있는 input과 다른 의미
-      // input tag에 있는 input은 키보드로 입력하는 것을 의미하는 input
-      // $emit에 있는 input은 컴포넌트간에 대화하기 위한 수단
-      const eventTarget = event.target as HTMLInputElement;
-      this.$emit('input', eventTarget.value);
-    },
-    addTodo() {
-      this.$emit('add')
-    },
-  }
+const props = defineProps({
+  // item: {
+  //   type: String,
+  //   required: true
+  // }
+  item: String
 })
+
+const handleInput = (event: InputEvent) => {
+  // input tag에 있는 input Event하고 $emit에 있는 input과 다른 의미
+  // input tag에 있는 input은 키보드로 입력하는 것을 의미하는 input
+  // $emit에 있는 input은 컴포넌트간에 대화하기 위한 수단?연결체!
+  const eventTarget = event.target as HTMLInputElement;
+  // defineEmits('inputtext', eventTarget.value);
+  // defineEmits('inputtext', eventTarget.value);
+}
+
+const addTodo = () => {
+  // this.$emit('add')
+  // defineEmits(['add']);
+  defineEmits(['add'])
+
+}
+
+
+
+// export default defineComponent({
+//   name: 'TodoInput',
+//   props: {
+//     item: {
+//       type: String,
+//       required: true
+//     }
+//   },
+//   methods: {
+//     handleInput(event: InputEvent) {
+//       // input tag에 있는 input Event하고 $emit에 있는 input과 다른 의미
+//       // input tag에 있는 input은 키보드로 입력하는 것을 의미하는 input
+//       // $emit에 있는 input은 컴포넌트간에 대화하기 위한 수단?연결체!
+//       const eventTarget = event.target as HTMLInputElement;
+//       this.$emit('inputtext', eventTarget.value);
+//     },
+//     addTodo() {
+//       this.$emit('add')
+//     },
+//   }
+// })
 </script>
